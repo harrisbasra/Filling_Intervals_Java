c6b44a5cd933d2438b910ddf4733e396120dddf4
Harris Basra <harrisbasra@gmail.com>
2023-10-08 23:32:12 +0500

    Interval Merge Made

--- src/com/mergedintervals/java/Interval_Merger.java
+++ src/com/mergedintervals/java/Interval_Merger.java
@@ -1,4 +1,35 @@
 package com.mergedintervals.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Interval_Merger {
+
+    public static List<int[]> mergeIntervals(List<int[]> intervals) {
+        List<int[]> mergedIntervals = new ArrayList<>();
+
+        if (intervals == null || intervals.isEmpty()) {
+            return mergedIntervals;
+        }
+
+        int[] currentInterval = intervals.get(0);
+
+        for (int i = 1; i < intervals.size(); i++) {
+            int[] nextInterval = intervals.get(i);
+
+            if (currentInterval[1] >= nextInterval[0]) {
+                // Merge the overlapping intervals
+                currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
+            } else {
+                // Add the current merged interval and update currentInterval
+                mergedIntervals.add(currentInterval);
+                currentInterval = nextInterval;
+            }
+        }
+
+        // Add the last merged interval
+        mergedIntervals.add(currentInterval);
+
+        return mergedIntervals;
+    }
 }
