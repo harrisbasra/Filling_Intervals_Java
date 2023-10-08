64623d97903583eb84e22efe0d7c46a03550856f
Harris Basra <harrisbasra@gmail.com>
2023-10-08 23:43:53 +0500

    Intervals Working

--- src/com/mergedintervals/java/Interval_Merger.java
+++ src/com/mergedintervals/java/Interval_Merger.java
@@ -4,6 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Interval_Merger {
+    public static void mergeIntervals(List<int[]> intervals) {
+        if (intervals == null || intervals.size() <= 1) {
+            return;
+        }
 
+        int i = 0;
+        while (i < intervals.size() - 1) {
+            int[] currentInterval = intervals.get(i);
+            int[] nextInterval = intervals.get(i + 1);
+
+            if (currentInterval[1] >= nextInterval[0]) {
+                // Merge the overlapping intervals
+                currentInterval[1] = Math.max(currentInterval[1], nextInterval[1]);
+                // Remove the next interval
+                intervals.remove(i + 1);
+            } else {
+                i++;
+            }
+        }
+    }
 
 }
