b2c07b8bb8ab764a9be0ff3673f62db597a2d876
Harris Basra <harrisbasra@gmail.com>
2023-10-08 23:44:09 +0500

    Test Class Defined

--- src/com/mergedintervals/java/Main.java
+++ src/com/mergedintervals/java/Main.java
@@ -5,6 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+        List<int[]> intervals = new ArrayList<>(Arrays.asList(
+                new int[]{1, 2},
+                new int[]{3, 6},
+                new int[]{8, 10},
+                new int[]{15, 18}
+        ));
 
+        Interval_Merger.mergeIntervals(intervals);
+
+        for (int[] interval : intervals) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
 }
 
